27cd997e7583e6e94863165d9e8b338a8d76fbd5
pedro <pedro.rittner@outlook.com>
2015-12-22 01:09:36 -0500

    Converted project to Maven, UJMP 3.0, dropped JAMA
    Maven is now used to grab dependencies and build a JAR for the whole
    project.
    
    Project was re-organized, moving files into proper hierarchy (e.g. all
    source files find themselves in the src/ folder and its subfolders).
    
    Various *_backups were removed, as were most .class files since these
    are BINARY files that ARE NOT (generally) COMPATIBLE BETWEEN
    JREs. Furthermore, maven will generate these binary files for us.
    
    Added CSI DM3 to Plugins-> menu to test integrating CSI directly into
    the ImageJ system.

---
+++ src/plugins/CSI/Open_Documentation.java
@@ -0,0 +1,56 @@
+package plugins.CSI;
+
+import java.awt.Desktop;
+import java.io.File;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class Open_Documentation implements PlugIn {
+
+	public void run(String arg) {
+	
+	/* To open a PDF using the plugin-----------------------*/
+		 String fileName = "plugins"+ File.separator + "CSI" + File.separator + "Documentation.pdf";
+		try {
+			 
+			File pdfFile = new File(fileName);
+			if (pdfFile.exists()) {
+	 
+				if (Desktop.isDesktopSupported()) {
+					Desktop.getDesktop().open(pdfFile);
+				} else {
+					IJ.showMessage("AWT Desktop is not supported!");
+				}
+	 
+			} 
+			else {
+				IJ.showMessage("The file "+ fileName + " does not exist!");
+			}
+	 
+			IJ.showMessage(fileName);
+	 
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		/*--------To open a URL--------------------
+		
+		 try {		
+			Process p=Runtime.getRuntime().exec("cmd /c start http://bit.ly/Nv2kMf");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+			
+		------------------------*/
+
+		
+		
+		
+	}
+	
+
+}
